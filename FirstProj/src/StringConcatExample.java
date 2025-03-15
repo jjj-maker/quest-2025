8b91179fb9fd2a520896ebe83132e5e2bc8cf3c6
TJ <TJ@DESKTOP-8FSFFJK>
2025-03-15 17:15:01 +0900

    aaa

---
+++ FirstProj/src/StringConcatExample.java
@@ -0,0 +1,16 @@
+
+public class StringConcatExample {
+
+	public static void main(String[] args) {
+		String str1 = "Java";
+		String str2 = "JDK";
+		
+		String str3 = str1 + 3 + 3.0;
+		String str4 = 3 + 3.0 + str2;
+		
+		System.out.println(str3);
+		System.out.println(str4);
+ 
+	}
+
+}
---
+++ FirstProj/src/q510/Main.java
@@ -0,0 +1,14 @@
+package q510;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.print("any 3 numbers:");
+		
+		
+
+	}
+
+}
