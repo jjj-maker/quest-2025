3cf79947ce809142ac447738577535f45896d0c5
TJ <TJ@DESKTOP-8FSFFJK>
2025-03-15 17:31:19 +0900

    aaa

---
+++ FirstProj/src/q511/Main.java
@@ -0,0 +1,24 @@
+package q511;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+        int num1, num2;
+        num1 = 55;
+        num2 = 10;
+        
+        		
+		 System.out.println(num1 + "-" + num2 + "=" + (num1- num2));
+		
+		 num1 = 2008;
+	     num2 = 1999;
+		 
+	     System.out.println(num1 + "-" + num2 + "=" + (num1- num2));
+		
+		
+		 
+
+	}
+
+}
