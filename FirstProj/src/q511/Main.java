64268bce72fc7e95c2f41e2a20084897aad9898b
TJ <TJ@DESKTOP-8FSFFJK>
2025-03-15 17:34:56 +0900

    aaa

--- FirstProj/src/q511/Main.java
+++ FirstProj/src/q511/Main.java
@@ -4,17 +4,17 @@ public class Main {
 
 	public static void main(String[] args) {
 
-        int num1, num2;
-        num1 = 55;
-        num2 = 10;
+         int num1, num2;
+         num1 = 55;
+         num2 = 10;
         
         		
-		 System.out.println(num1 + "-" + num2 + "=" + (num1- num2));
+		 System.out.println(num1 + " - " + num2 + " = " + (num1- num2));
 		
 		 num1 = 2008;
 	     num2 = 1999;
 		 
-	     System.out.println(num1 + "-" + num2 + "=" + (num1- num2));
+	     System.out.println(num1 + " - " + num2 + " = " + (num1- num2));
 		
 		
 		 
